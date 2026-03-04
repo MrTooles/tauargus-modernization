b7c32d4fe7261492fce64e3808e1e0750840d058
eluft001 <elija.luft@student.hs-rm.de>
2026-03-04 12:09:11 +0100

    Migration to Java 11. added /out/ to gitignore

--- src/tauargus/model/Application.java
+++ src/tauargus/model/Application.java
@@ -143,7 +143,7 @@ public class Application {
     static{
         try{
             //ClassPathHack.addFile(getSpssVersion() + "\\spssjavaplugin.jar");
-            ClassPathHack.loadLibrary(new File(getSpssVersion() + "\\spssjavaplugin.jar"));
+            //ClassPathHack.loadLibrary(new File(getSpssVersion() + "\\spssjavaplugin.jar"));
         }//catch (IOException ex){System.out.print(ex.toString());}
         catch (Exception ex){System.out.print(ex.toString());}
     }
